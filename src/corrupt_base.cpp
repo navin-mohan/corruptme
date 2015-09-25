0264176124ab21761c5e2fda3c46426c24987f14
Navin Mohan <navinmohan81@gmail.com>
2015-09-25 19:43:14 +0530

    The base class is set up and tested. The new methods header added. New corruption methods can be derived form the base class. RandomCharsMethod is one such method writted by Arjunair.It is also added.

--- src/corrupt_base.cpp
+++ src/corrupt_base.cpp
@@ -1,11 +1,29 @@
 #include "corrupt_base.h"
 #include <fstream>
 
-corruptme::corruptfile::corruptfile(std::fstream f)
-{
-  file = f;
-}
 corruptme::corruptfile::corruptfile(std::string filename){
-  file = fstream(filename.c_str(),std::ios::in | std::ios::out | std::ios::binary);
+  if (isBinary)
+    file.open(filename.c_str(),std::ios::in | std::ios::out | std::ios::binary);
+  else
+    file.open(filename.c_str(),std::ios::in | std::ios::out);
+  file_open = true;
 }
-corruptme::corruptfile::corruptfile(void){}
+corruptme::corruptfile::corruptfile(void){
+  file_open = false;
+  c_file_open = false;
+}
+
+corruptme::corruptfile::corruptfile(FILE *f){
+  file_c = f;
+  c_file_open = true;
+}
+
+corruptme::corruptfile::corruptfile(char* filename){
+  file_c = fopen(filename,"r+");
+  c_file_open = true;
+}
+
+corruptme::corruptfile::~corruptfile(){
+  if(c_file_open) fclose(file_c);
+  if(file_open) file.close();
+}
--- src/corrupt_base.h
+++ src/corrupt_base.h
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cstdio>
 
 #ifndef CORRUPTBASE_H
 #define CORRUPTBASE_H
@@ -7,13 +8,19 @@ namespace corruptme
 {
   class corruptfile
   {
-    std::fstream file;
 
+  protected:
+
+    std::fstream file;    //c++ filestream object
+    FILE *file_c;         //c style file ptr
+    bool c_file_open,file_open;  //keep track of open files
 
   public:
-    corruptfile(std::fstream f);
-    corruptfile(std::string filename);
-    corruptfile(void);
+    corruptfile(std::string filename,bool isBinary = true); //to asscociate the file obj with the file filename
+    corruptfile(FILE *f);         //to init the c file ptr with FILE* f
+    corruptfile(char* filename);  //to create a FILE ptr to the file filename
+    corruptfile(void);            // default ctor
+    ~corruptfile();               //dtor : closes all open file streams
 
   };
 }
